1d140bc93a3b8fa87e04e6a57de60f2479037039
Ronale <ronalework@gmail.com>
2025-07-22 20:33:04 -0300

    pratica 01

---
+++ pratica01/caixa_retangular.py
@@ -0,0 +1,12 @@
+# Dimensões da caixa retangular
+comprimento = 12  # cm
+largura = 14      # cm
+altura = 20       # cm
+
+# Cálculo do volume
+volume = comprimento * largura * altura
+
+# Exibição do resultado em cm³
+# O resultado abaixo mostra o volume calculado da caixa retangular em centímetros cúbicos.
+print(f"volume = {comprimento} * {largura} * {altura} = {volume} ")
+print(f"O volume da caixa retangular é: {volume} cm³")
